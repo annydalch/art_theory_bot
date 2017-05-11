28289033798e5b2bf97a9d2fa2a5fac5a32399b0
Arthur Goldman <arthur@goldman-tribe.org>
2017-05-11 12:40:37 -0500

    ?

--- src/lib.rs
+++
@@ -1,2 +0,0 @@
-extern crate oauthcli;
-extern crate url;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+
+const FILENAME: &str = "../../resources/big_list_of_crap.txt";
+
 fn main() {
-    println!("Hello, world!");
+    read_from_file(FILENAME);
+}
+
+fn read_from_file(filename: &str) {
+    let file = File::open(filename).unwrap();
+    let mut buf_reader = BufReader::new(file);
+    let mut contents = String::new();
+    buf_reader.read_to_string(&mut contents);
+    println!("{}", contents);
 }
